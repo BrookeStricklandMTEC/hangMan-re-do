2069b5bf3976cf2e31ff73ae7c7ad14bb9ebe519
BROOKE.STRICKLAND@STU.MTEC.EDU <BROOKE.STRICKLAND2904@STU.MTEC.EDU>
2022-04-07 23:44:32 -0600

    first commit

---
+++ Week5Day4Project/app.js
@@ -0,0 +1,18 @@
+
+$('.navItem').css("color" , "white");
+$('#nav').css("background-color" , "#145"); 
+$('tr.even').css("background-color" , "#ccc");
+$('tr.odd').css("background-color" , "#aaa");
+
+let traverse1 = $('#inputTag').children()[3]; 
+console.log(traverse1);
+
+let traverse2 = $('#inputTag').children[1];
+console.log(traverse2);
+
+$('#inputTag').text('Brooke'); 
+
+
+
+
+
